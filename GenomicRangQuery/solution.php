e4bb90ff390e83552f733c0b7c22ca3976cd35dd
Akbar Dizaji <vatantech724@gmail.com>
2023-01-06 05:59:08 -0800

    Add correct solution of GenomicRangQuery

--- GenomicRangQuery/solution.php
+++ GenomicRangQuery/solution.php
@@ -1,4 +1,36 @@
 <?php
+function solution($S, $P, $Q) {
+    $sizeOfS = strlen($S);
+ $sizeOfP = sizeof($P);
+ $impact_factor = array('A' => 1, 'C' => 2, 'G' => 3, 'T' => 4);
+ for($i=0; $i<$sizeOfS; $i++){
+     foreach($impact_factor as $key => $value){
+         if($i == 0){
+             $prefix_sum[$key][$i] = ($S[$i] == $key) ? 1 : 0;
+         }else{
+             if($S[$i] == $key)
+                 $prefix_sum[$key][$i] = $prefix_sum[$key][$i-1] + 1;
+             else
+                 $prefix_sum[$key][$i] = $prefix_sum[$key][$i-1];
+         }
+     }
+ }
+ for($i=0; $i<$sizeOfP; $i++){
+     foreach($impact_factor as $key => $value){
+         if($Q[$i]==$P[$i]){
+             $result[$i] = $impact_factor[$S[$Q[$i]]];
+             break;
+         }elseif($P[$i]-1 >= 0 && $prefix_sum[$key][$Q[$i]]-$prefix_sum[$key][$P[$i]-1]>0){
+             $result[$i] = $impact_factor[$key];
+             break;
+         }elseif($P[$i]-1 < 0 && $prefix_sum[$key][$Q[$i]] > 0){
+             $result[$i] = $impact_factor[$key];
+             break;
+         }
+     }
+ }
+ return $result;
+}
 
 
 
